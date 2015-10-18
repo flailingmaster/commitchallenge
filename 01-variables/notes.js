04a556e37b2e87b462162b00cf8a3ff8c5caac20
Raymond Wang <raymond.wang@acquia.com>
2015-10-18 12:52:46 -0500

    test

--- 01-variables/notes.js
+++ 01-variables/notes.js
@@ -1,2 +1,9 @@
 // your notes here!
 console.log("hello world!");
+
+var burritoPrice = 10;
+var tax = 1;
+var novalue;
+var totalCost = burritoPrice + tax;
+console.log(burritoPrice);
+console.log(novalue);
