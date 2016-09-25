c2268b90655991778a056e55261fdee5e3008a73
Raymond Wang <raymond@blinkingtwelve.net>
2016-09-24 22:35:20 -0700

    another day another function

--- javascript-path/04-conditionals/notes.js
+++ javascript-path/04-conditionals/notes.js
@@ -1,2 +1,44 @@
 // your notes here!
 console.log("hello world!");
+
+//
+//      //=> aardvark
+//
+//
+//      //=> whale
+//
+//
+//      //=> aardvark
+//
+// It should throw an error string if any of the arguments are not strings.
+//
+//
+//      //=> ALL THREE ARGS MUST BE STRINGS!
+
+var firstInDictionary = function (a, b, c) {
+  if (typeof a == "string" && typeof b == "string" && typeof c == "string") {
+    console.log("looks ok");
+    if (a < b) {
+      if (a < c) {
+        console.log(a);
+        return a;
+      } else {
+        console.log(c);
+        return c;
+      }
+    } else if (b < c) {
+      console.log(b);
+      return b;
+    } else {
+      console.log(c);
+      return c;
+    }
+  } else {
+    throw "ALL THREE ARGS MUST BE STRINGS!";
+  }
+};
+
+firstInDictionary("rhino", "aardvark", "zebra");
+firstInDictionary("whale", "zebra", "yak");
+firstInDictionary("whale", "zebra", "aardvark");
+firstInDictionary("whale", 5, 10);
--- javascript-path/04-conditionals/practice.js
+++ javascript-path/04-conditionals/practice.js
@@ -85,7 +85,27 @@ var isLeapYear = function (year) {
 //
 //      firstInDictionary("whale", 5, 10);
 //      //=> ALL THREE ARGS MUST BE STRINGS!
-var firstInDictionary = function () {
+var firstInDictionary = function (a, b, c) {
+  if (typeof a == "string" && typeof b == "string" && typeof c == "string") {
+    console.log("looks ok");
+    if (a < b) {
+      if (a < c) {
+        console.log(a);
+        return a;
+      } else {
+        console.log(c);
+        return c;
+      }
+    } else if (b < c) {
+      console.log(b);
+      return b;
+    } else {
+      console.log(c);
+      return c;
+    }
+  } else {
+    throw "ALL THREE ARGS MUST BE STRINGS!";
+  }
 };
 
 
